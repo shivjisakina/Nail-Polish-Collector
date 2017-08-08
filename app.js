d8106b4fad4edf98452ac195567654afbc2247df
Sakina Shivji <shivjisakina@gmail.com>
2017-08-07 22:33:07 -0400

    cleaned up picture sizes, started js

---
+++ app.js
@@ -0,0 +1,23 @@
+// GLOBAL VARIABLES
+// =================================================================
+
+var polish = {
+
+    purple: {
+        name: "purple",
+        value: 0
+    },
+    green: {
+        name: "green",
+        value: 0
+    },
+    maroon: {
+        name: "maroon",
+        value: 0
+    },
+    white: {
+        name: "white",
+        value: 0
+    }
+
+}
